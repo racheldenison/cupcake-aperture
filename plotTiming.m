d7b859608c2ad78997ccdd693b04125f0a2e6875
Rachel Denison <r.n.denison@gmail.com>
2019-04-04 15:28:52 -0400

    updated plotTiming.m

--- plotTiming.m
+++ plotTiming.m
@@ -1,5 +1,6 @@
 function plotTiming(expt)
 
+p = expt.p;
 trials_headers = expt.trials_headers;
 trials = expt.trials;
 timing = expt.timing;
@@ -9,9 +10,17 @@ itiActual = [NaN timing.iti'];
 itiActual(itiActual > 2) = NaN; % between blocks
 
 figure
+subplot(1,2,1)
+hold on
+histogram(timing.imDur)
+vline(p.gratingDur,'k')
+xlabel('Image duration (s)')
+ylabel('Number of trials')
+
+subplot(1,2,2)
 hold on
 plot([.5 1.5],[.5 1.5])
 scatter(itiRequested, itiActual)
-xlabel('Requested ITI')
-ylabel('Actual ITI')
+xlabel('Requested ITI (s)')
+ylabel('Actual ITI (s)')
 axis square
