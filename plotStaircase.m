01d799f55dc9ed1a6fb0337a3996496a8be538aa
Rachel Denison <r.n.denison@gmail.com>
2019-04-11 17:33:04 -0400

    added plotStaircase, rd_eyeLink, rd_saveAllFigs

---
+++ plotStaircase.m
@@ -0,0 +1,27 @@
+function plotStaircase(expt, saveFigs)
+
+subjectID = expt.subjectID;
+run = expt.run;
+
+p = expt.p;
+stairValues = expt.staircase.stairValues;
+
+for i = 1:numel(stairValues)
+    vals(i) = p.stairs(stairValues(i));
+end
+
+figure('Position',[300 300 930 350])
+subplot(1,2,1)
+plot(vals)
+xlabel('trial')
+ylabel('luminance')
+title('staircase')
+subplot(1,2,2)
+semilogy(1-vals)
+ylim([10^-1.5 1])
+xlabel('trial')
+ylabel('1 - log(luminance)')
+
+if saveFigs
+    rd_saveAllFigs(gcf, {'staircase'}, sprintf('%s_run%02d', subjectID, run))
+end
